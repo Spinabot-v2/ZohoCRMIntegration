54a58107f950bbb832dc7967076427db98eb35aa
Parth <parthdande15@gmail.com>
2025-05-12 21:04:10 +0530

    Add Redis configuration and update deployment settings

--- config.py
+++ config.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads variables from .env into os.environ
 
 class Config:
-    client_id = os.getenv("ZOHO_CLIENT_ID")
-    client_secret = os.getenv("ZOHO_CLIENT_SECRET")
-    database_name = os.getenv("DEV_DATABASE_NAME")
-    database_user = os.getenv("DEV_DB_USER")
-    database_pass = os.getenv("DEV_DB_PASSWORD")
-    database_host = os.getenv("DEV_CONNECTION_NAME")
-    SECRET_KEY = 'SECRET-KEY-FLASK-APP'
+    client_id = os.getenv("ZOHO_CLIENT_ID", "default_client_id")
+    client_secret = os.getenv("ZOHO_CLIENT_SECRET", "default_client_secret")
+    redirect_uri = os.getenv("ZOHO_REDIRECT_URI", "default_redirect_uri")
+    database_name = os.getenv("DEV_DATABASE_NAME", "default_database_name")
+    database_user = os.getenv("DEV_DB_USER", "default_db_user")
+    database_pass = os.getenv("DEV_DB_PASSWORD", "default_db_password")
+    database_host = os.getenv("DEV_CONNECTION_NAME", "default_connection_name")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+    redis_host = os.getenv("DEV_CACHE_REDIS_HOST", "default_redis_host")
+    redis_port = os.getenv("DEV_CACHE_REDIS_PORT", "default_redis_port")
+    redis_username = os.getenv("DEV_CACHE_REDIS_USERNAME", "default_redis_username")
+    redis_password = os.getenv("DEV_CACHE_REDIS_PASSWORD", "default_redis_password")
--- utils/extension.py
+++ utils/extension.py
@@ -2,10 +2,13 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import redis
-
-redis_client = redis.Redis(host='redis', port=6379, db=0)
+from config import Config 
+redis_client = redis.Redis(
+    host=Config.redis_host,
+    port=Config.redis_port,
+    db=0)
 
 limiter = Limiter(
     key_func=get_remote_address,
-    storage_uri="redis://redis:6379"
+    storage_uri=f"redis://{Config.redis_host}:{Config.redis_port}"
 )
