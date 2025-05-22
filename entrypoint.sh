cc148b5e4a3ad8935fa0e877057a9acf7369d5b1
Parth <parthdande15@gmail.com>
2025-05-23 00:28:50 +0530

    Remove hardcoded port info from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+# Gunicorn configuration
 # Wait for PostgreSQL
 echo "Waiting for PostgreSQL..."
-while ! nc -z postgres 5432; do
+while ! nc -z postgres "$DB_PORT"; do
   sleep 1
 done
 echo "PostgreSQL is up!"
 
 # Wait for Redis
 echo "Waiting for Redis..."
-while ! nc -z redis 6379; do
-
+while ! nc -z redis "$CACHE_REDIS_PORT"; do
   sleep 1
 done
 echo "Redis is up!" 
@@ -19,6 +19,7 @@ python create_db.py
 
 echo "Running migrations..."
 mkdir -p migrations/versions
+
 alembic upgrade head
 
 echo "Initializing database..."
@@ -26,6 +27,10 @@ echo "Initializing database..."
 export FLASK_APP=main.py
 # Run the Flask CLI command to initialize the database
 flask db init
+
+
+
+
 echo "Starting application..."
 # Start the application with Gunicorn using env vars
 gunicorn -w "$GUNICORN_WORKERS" -k "$GUNICORN_CLASS" -b "$GUNICORN_HOST" main:app
